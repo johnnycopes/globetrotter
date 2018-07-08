372d2cd5d97b2e1759490a855cc5790be086d1c5
John Coppola <jcoppola@instien.com>
2018-07-08 19:42:22 -0400

    improves selection screen with fade transition, css grid division of selections, and icons for regions. refactors quiz page to use grid to display up to 5 quiz cards per row

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import {
+  trigger,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 import { SelectService } from './select.service';
 import { Selection, CountryTally, Quantity, QuantityModel } from '../shared/model/select.interface';
@@ -7,7 +13,15 @@ import { Selection, CountryTally, Quantity, QuantityModel } from '../shared/mode
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.scss']
+  styleUrls: ['./select.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: '0' }),
+        animate('300ms ease-in', style({ opacity: '1' }))
+      ])
+    ])
+  ]
 })
 export class SelectComponent implements OnInit {
   @Output() selectionMade = new EventEmitter<Selection>();
