9797f867ed2bb477dea4e4538c204f5c44b7dfbe
John Coppola <jcoppola@instien.com>
2018-07-13 19:14:06 -0400

    properly configures button onClick action and finishes prepping inputs to be extracted into a reusable component

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -11,6 +11,7 @@ export class ButtonComponent implements OnInit {
   @Input() buttonStyle: string;
   @Input() buttonSize?: string;
   @Input() isDisabled?: boolean;
+  @Output() onClick: EventEmitter<Event> = new EventEmitter<Event>();
 
   constructor() { }
 
