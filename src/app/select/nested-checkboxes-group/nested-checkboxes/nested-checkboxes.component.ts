4b109d7e75b7290c3929ed3d6ddd93865c36ae95
John Coppola <jcoppola@instien.com>
2018-08-14 13:00:30 -0400

    makes more progress on the individual nested-checkboxes component and leaves better TODO notes about what needs to be accomplished in various files

--- src/app/select/nested-checkboxes-group/nested-checkboxes/nested-checkboxes.component.ts
+++ src/app/select/nested-checkboxes-group/nested-checkboxes/nested-checkboxes.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
 
-import { Tally } from '../../../shared/model/select.interface';
-import { NestedCheckboxesGroupService } from '../nested-checkboxes-group.service';
+import { Tally } from 'src/app/shared/model/select.interface';
 
 @Component({
   selector: 'app-nested-checkboxes',
@@ -15,25 +13,54 @@ export class NestedCheckboxesComponent implements OnInit {
   @Input() dataByCategory: _.Dictionary<any[]>;
   @Input() dataBySubcategory: _.Dictionary<any[]>;
   @Input() tally: Tally;
+  public categoryModel: any; // TODO: define this
 
-  constructor(private nestedCheckboxesGroupService: NestedCheckboxesGroupService) { }
+  constructor() { }
 
   ngOnInit() {
-    console.log(`${this.category}:`);
+    console.group(`${this.category}:`);
     console.log('data by category:', this.dataByCategory);
     console.log('data by subcategory:', this.subcategories);
     console.log('tally:', this.tally);
+    console.groupEnd();
+
+    /*
+    * TODO:
+    * - initialize the form in a cleaner way
+    * - extract the useful functionality out of the nested-checkboxes-group service
+    * - make this component responsible for generating/maintaining its own tally
+    */
+    this.categoryModel = {
+      category: this.category,
+      checkboxState: true,
+      subcategories: {}
+    };
+    this.initializeModel();
+  }
+
+  initializeModel() {
+    this.subcategories.forEach(subcategory => {
+      this.categoryModel.subcategories[subcategory] = true;
+    });
   }
 
   onCategoryChange(category: HTMLInputElement) {
     console.log('category change', category);
+
+    // TODO: make this toggle all the subcategories' values to be all true or all false at once
+
+    // (ORIGINAL CODE BELOW)
   //   const subcategories = this.subcategoriesByCategory[category.value];
   //   const updateToFormModel = this.nestedCheckboxesGroupService.createCategoryAndSubcategoryUpdate(category.value, subcategories, category.checked);
   //   this.form.patchValue(updateToFormModel);
   }
 
-  onSubcategoryChange(subcategory: HTMLInputElement) {
-    console.log('subcategory change:', subcategory)
+  onSubcategoryChange(category: HTMLInputElement) {
+    console.log('subcategory change:', category);
+
+    // TODO: make this toggle the indeterminate state of the category checkbox as needed
+
+    // (ORIGINAL CODE BELOW)
   //   const updateToFormModel = this.nestedCheckboxesGroupService.createCategoryUpdate(this.form, category.value, this.subcategoriesByCategory[category.value]);
   //   this.form.patchValue(updateToFormModel);
   }
