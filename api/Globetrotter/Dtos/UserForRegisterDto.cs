3f39d28c5756af59b2c87593087185fe9ad20a7d
John Coppola <jcoppola@insiten.com>
2019-11-29 15:42:59 -0500

    corrects min length in API

--- api/Globetrotter/Dtos/UserForRegisterDto.cs
+++ api/Globetrotter/Dtos/UserForRegisterDto.cs
@@ -8,7 +8,7 @@ namespace Globetrotter.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(20, MinimumLength = 4, ErrorMessage ="Password must be between 8 and 20 characters")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage ="Password must be between 8 and 20 characters")]
         public string Password { get; set; }
     }
 }
