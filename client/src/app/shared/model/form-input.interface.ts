2b284a0177b929d85a8aa385d860f4aed9963b90
John Coppola <jcoppola@insiten.com>
2019-12-01 13:38:12 -0500

    adds no-unused-variables to tslint; deletes custom form-input interface; adds automatically-generated launch.json and tasks.json files

--- client/src/app/shared/model/form-input.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Validators } from "@angular/forms";
-
-export interface FormInput {
-  name: string,
-  type: string,
-  label: string,
-  validators?: Validators[];
-  errorMessage?: string;
-}
