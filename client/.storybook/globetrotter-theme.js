39b0d19876774d72e0fab1adf492786409dcff59
John Coppola <jcoppola@insiten.com>
2019-12-26 16:25:49 -0500

    restores globetrotter.svg file for Storybook; adds loader and modal stories files

--- client/.storybook/globetrotter-theme.js
+++ client/.storybook/globetrotter-theme.js
@@ -2,8 +2,7 @@ import { create } from '@storybook/theming';
 
 export default create({
   base: 'dark',
-
   brandTitle: 'Globetrotter',
   brandUrl: 'https://globetrotter.surge.sh/',
-  brandImage: 'assets/icons/Globetrotter.svg',
+  brandImage: 'assets/Globetrotter.svg',
 });
