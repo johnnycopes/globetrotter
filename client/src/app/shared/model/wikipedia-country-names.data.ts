915e7b59206dc7fa0731a4c3df1bfad22b2ee4d1
John Coppola <jcoppola@insiten.com>
2020-03-20 08:53:53 -0400

    finalizes info table usage; simplifies observable usage in explore; adds wiki country lookup dict

---
+++ client/src/app/shared/model/wikipedia-country-names.data.ts
@@ -0,0 +1,7 @@
+// List of country names that fetch correct data in Wikipedia
+
+export const WIKIPEDIA_COUNTRIES: _.Dictionary<string> = {
+  'Georgia': 'Georgia country',
+  "Korea (Democratic People's Republic of)": 'North Korea',
+  "Macedonia (the former Yugoslav Republic of)": 'North Macedonia'
+};
