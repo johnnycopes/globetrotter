2e5117da31fc4daec1ca5bcfade5b88993254392
John Coppola <jcoppola@insiten.com>
2021-01-16 10:04:03 -0500

    deletes .spec files and other testing files + commands; corrects ts version in package.json

--- client/src/app/modules/shared/pipes/measurement.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MeasurementPipe } from './measurement.pipe';
-
-describe('MeasurementPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MeasurementPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
