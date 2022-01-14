295eecbba1c382c3d5d228e6296efca8fa47182f
John Coppola <jdcoppola@gmail.com>
2022-01-13 21:53:00 -0700

    deletes API projects

--- api_old/Globetrotter/Dtos/UserForLoginDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Globetrotter.Dtos
-{
-    public class UserForLoginDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
--- api_old/Globetrotter/Models/Value.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Globetrotter.Models
-{
-    public class Value
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
